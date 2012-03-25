28e27ea8222bf24d1f6cda6606ac154e3ecd6fe1
Erik Sundén <erik.sunden@liu.se>
2012-03-25 23:53:30 +0000

    vis12-fem: Renamed misspelled folder.
    git-svn-id: https://scivis.itn.liu.se/svn/publications/workinprogress/vis12-fem@3735 cd0c9ba3-fe29-0410-b0c2-9a6eeba77867

---
+++ publish.bat
@@ -0,0 +1,27 @@
+
+@echo off
+REM Install Ghostscript/MikTex (for ps2pdf) and XPDF (for pdftops) (Make sure ps2pdf and pdftops can be accessed through path)
+REM http://pages.cs.wisc.edu/~ghost/
+REM http://foolabs.com/xpdf/download.html
+
+:spec_file
+echo Finalize publication PDF (flattening, embed fonts, cmyk)
+set file=vis12-fem
+
+:start
+echo.
+echo Converting to PS
+echo.
+
+pdftops -paper match %file%.pdf %file%.ps
+
+echo.
+echo Converting to PDF
+echo.
+
+ps2pdf -dPDFSETTINGS#/prepress -dEmbedAllFonts#true -dProcessColorModel#/DeviceCMYK %file%.ps %file%-publish.pdf
+
+echo Done! - New PDF: %file%-publish.pdf
+echo.
+
+pause
